fda36bd88cb93707ec115b22e5b73f63449c9005
TravisGray1 <travisogray1@gmail.com>
2020-03-08 13:38:18 -0400

    Commit 1

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {
+	database: {
+		host:	  '127.0.0.1', 	// database host
+		user: 	  'root', 		// your database username
+		password: ' ', 		// your database password
+		port: 	  3306, 		// default MySQL port
+		db: 	  'stor4DB' 		// your database name
+	},
+	server: {
+		host: '127.0.0.1',
+		port: '3000'
+	}
+}
+module.exports = config
